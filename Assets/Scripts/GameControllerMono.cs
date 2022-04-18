68fa57ba9fae001ea37276ccabe9fbe883da0fcf
erkan <etaylan13@gmail.com>
2022-04-18 20:09:10 +0300

    init

---
+++ Assets/Scripts/GameControllerMono.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace MoonGames.Game.FrogJump
+{
+    public class GameControllerMono : MonoBehaviour
+    {
+        public Camera mainCamera;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectObject();
+            }
+        }
+
+        private void SelectObject()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(Input.mousePosition);
+            bool raycast = Physics.Raycast(ray, out RaycastHit hit);
+            if (!raycast)
+            {
+                return;
+            }
+
+            if (!hit.transform.gameObject.TryGetComponent(out SelectableMono selectable))
+            {
+                return;
+            }
+
+            selectable.SelectOrUnselect();
+        }
+    }
+}
---
+++ Assets/Scripts/SelectableMono.cs
@@ -0,0 +1,18 @@
+﻿using cakeslice;
+using UnityEngine;
+
+namespace MoonGames.Game.FrogJump
+{
+    public class SelectableMono : MonoBehaviour
+    {
+        public bool isSelected;
+        private Outline outline;
+
+        public void SelectOrUnselect()
+        {
+            outline ??= gameObject.AddComponent<Outline>();
+            outline.enabled = !isSelected;
+            isSelected = !isSelected;
+        }
+    }
+}
